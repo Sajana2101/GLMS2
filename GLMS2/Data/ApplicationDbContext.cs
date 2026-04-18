c1a64514ae7681c8f3ca144b4b3a6b91bc8d2e62
Sajana2101 <ST10433940@vcconnect.edu.za>
2026-04-18 17:22:08 +0200

    Created data folder and the applciationdbcontext. nb this was also copied over

---
+++ GLMS2/Data/ApplicationDbContext.cs
@@ -0,0 +1,47 @@
+﻿using GLMS2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GLMS2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<Contract> Contracts { get; set; }
+
+        public DbSet<ServiceRequest> ServiceRequests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Client>()
+                .HasMany(c => c.Contracts)
+                .WithOne(c => c.Client)
+                .HasForeignKey(c => c.ClientId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Contract>()
+                .HasMany(c => c.ServiceRequests)
+                .WithOne(sr => sr.Contract)
+                .HasForeignKey(sr => sr.ContractId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<ServiceRequest>()
+                .Property(sr => sr.CostUSD)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<ServiceRequest>()
+                .Property(sr => sr.CostZAR)
+                .HasPrecision(18, 2);
+
+
+           
+        }
+    }
+}
