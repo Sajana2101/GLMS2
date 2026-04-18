82c3e17a2568b560f3d5b44a03c8aae6b6542f8b
Sajana2101 <ST10433940@vcconnect.edu.za>
2026-04-18 17:46:40 +0200

    Created database

---
+++ GLMS2/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using GLMS2.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace GLMS2.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=GLMSDB;Trusted_Connection=True;TrustServerCertificate=True;");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
