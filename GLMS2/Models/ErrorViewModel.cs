66b2ccb3b490e749307831c45e010370375ede74
Sajana2101 <ST10433940@vcconnect.edu.za>
2026-04-18 17:03:40 +0200

    Add project files.

---
+++ GLMS2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GLMS2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
