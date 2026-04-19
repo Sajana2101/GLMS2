d7d309c7eaf59115732d3edd32700c1d9cf913c7
Sajana2101 <ST10433940@vcconnect.edu.za>
2026-04-19 11:54:39 +0200

    Created the client controller along with the appropriate view pages

---
+++ GLMS2/Controllers/ClientController.cs
@@ -0,0 +1,163 @@
+﻿using GLMS2.Data;
+using GLMS2.Models;
+using GLMS2.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GLMS2.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Client
+        public async Task<IActionResult> Index()
+        {
+            var clients = await _context.Clients.ToListAsync();
+            return View(clients);
+        }
+
+        // GET: Client/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Clients
+                .Include(c => c.Contracts)
+                .FirstOrDefaultAsync(c => c.ClientId == id);
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return View(client);
+        }
+
+        // GET: Client/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Client/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ClientCreateViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var client = new Client
+            {
+                Name = model.Name,
+                Region = model.Region,
+                Email = model.Email
+            };
+
+            _context.Clients.Add(client);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Client/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Clients.FindAsync(id);
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            var model = new ClientCreateViewModel
+            {
+                Name = client.Name,
+                Region = client.Region,
+                Email = client.Email
+            };
+
+            ViewBag.ClientId = client.ClientId;
+            return View(model);
+        }
+
+        // POST: Client/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ClientCreateViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.ClientId = id;
+                return View(model);
+            }
+
+            var client = await _context.Clients.FindAsync(id);
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            client.Name = model.Name;
+            client.Region = model.Region;
+            client.Email = model.Email;
+
+            _context.Update(client);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Client/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Clients
+                .FirstOrDefaultAsync(c => c.ClientId == id);
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return View(client);
+        }
+
+        // POST: Client/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var client = await _context.Clients.FindAsync(id);
+
+            if (client != null)
+            {
+                _context.Clients.Remove(client);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- GLMS2/Program.cs
+++ GLMS2/Program.cs
@@ -1,33 +1,45 @@
 using GLMS2.Data;
+using GLMS2.Interfaces;
+using GLMS2.Services;
+using GLMS2.Services.Factories;
+using GLMS2.Services.Mediator;
 using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Database
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Dependency Injection - Services
+builder.Services.AddScoped<IFileService, FileService>();
+builder.Services.AddScoped<IContractFactory, ContractFactory>();
+builder.Services.AddScoped<IContractService, ContractService>();
+builder.Services.AddScoped<IServiceRequestService, ServiceRequestService>();
+builder.Services.AddScoped<IMediator, GLMSMediator>();
+
+// HttpClient for external currency API
+builder.Services.AddHttpClient<ICurrencyService, CurrencyService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
-
 app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Client}/{action=Index}/{id?}");
 
 app.Run();
