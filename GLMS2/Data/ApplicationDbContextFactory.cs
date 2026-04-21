cf3e378f056bd0177c5b02ffedba30eb9458f9db
Sajana2101 <ST10433940@vcconnect.edu.za>
2026-04-21 20:14:31 +0200

    added comments to the application db context factory

--- GLMS2/Data/ApplicationDbContextFactory.cs
+++ GLMS2/Data/ApplicationDbContextFactory.cs
@@ -4,14 +4,18 @@ using Microsoft.EntityFrameworkCore.Design;
 
 namespace GLMS2.Data
 {
+    // Provides a design-time DbContext for EF Core tools
+    // Ensures migrations can be created even when the application is not running
     public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
     {
         public ApplicationDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-
+            // Configures SQL Server connection for the GLMS database
+            // Supports Entity Framework Core migrations and database updates
             optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=GLMSDB;Trusted_Connection=True;TrustServerCertificate=True;");
-
+            // Returns configured DbContext used by EF Core tools
+            // Allows creation of tables for Client, Contract, and ServiceRequest entities
             return new ApplicationDbContext(optionsBuilder.Options);
         }
     }
