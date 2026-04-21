8ce825bc6207c015d7bb35407310829d9244c60c
Sajana2101 <ST10433940@vcconnect.edu.za>
2026-04-21 20:19:41 +0200

    added comments to the contract status Enum

--- GLMS2/Enums/ContractStatus.cs
+++ GLMS2/Enums/ContractStatus.cs
@@ -1,10 +1,19 @@
 ﻿namespace GLMS2.Enums
-{
+{ // Defines the workflow stages for a contract
+    // Used to control how contracts behave in the system and what actions are allowed
     public enum ContractStatus
     {
+        // Contract has been created but is not yet active
+        // Allows editing before the agreement is finalised
         Draft = 0,
+        // Contract is valid and operational
+        // Only Active contracts can have Service Requests created
         Active = 1,
+        // Contract is valid and operational
+        // Only Active contracts can have Service Requests created
         Expired = 2,
+        // Contract temporarily paused or unavailable
+        // Restricts workflow actions until status changes
         OnHold = 3
     }
 }
